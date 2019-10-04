1b5592b8010953415a6fa65dd4116e11b9f33844
Aryanna Anderson <tuh05038@temple.edu>
2019-10-04 00:24:44 -0400

    working on pipe execution. Used info from regular external command execution and information on pipes to create pipe execution

--- project2/pipeExecution.c
+++ project2/pipeExecution.c
@@ -10,6 +10,49 @@
 #include "shell.h"
 
 
-int pipeExecution(char **args){
-return 1;
+	int pipeExecution(char **args){
+	char **beforePipe;
+	char **afterPipe;
+	beforePipe=str_tok(args[0]," ");
+	afterPipe=str_tok(args[1]," ");
+	int pipefd[2];
+    	pid_t p1, p2;
+	if (pipe(pipefd) < 0) {
+        printf("\nPipe could not be initialized");
+        return 1;
+    	}
+    	p1 = fork();
+    	if (p1 < 0) {
+        printf("\nCould not fork");
+        return 1;
+    	}
+	if (p1 == 0) {
+
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+	if (execvp(beforePipe[0], beforePipe) < 0) {
+            printf("\nCould not execute command 1..");
+            return 1;
+        }
+    	} else {
+        p2 = fork();
+        if (p2 < 0) {
+            printf("\nCould not fork");
+            return 1;
+        }
+        if (p2 == 0) {
+            close(pipefd[1]);
+            dup2(pipefd[0], STDIN_FILENO);
+            close(pipefd[0]);
+            if (execvp(afterPipe[0], afterPipe) < 0) {
+                printf("\nCould not execute command 2..");
+                exit(0);
+            }
+        } else {
+            wait(NULL);
+            wait(NULL);
+        }
+    }
+	return 1;
 }

d7008b6855a226721ec276b1e8b9c55b46821712
Aryanna Anderson <tuh05038@temple.edu>
2019-10-06 14:17:20 -0400

    created makefile still working on background execution and redirection

---
+++ project2/externalCommands.c
@@ -0,0 +1,56 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+#include<dirent.h>
+#include "shell.h"
+
+
+
+
+
+        int externalCommands(struct simple_command com){
+        int fdin;
+        int fdout;
+        if(com.word==NULL){
+                printf("No command entered");
+                return 1;
+                }
+        if(com.inputRedirection==1){
+        fdin=open(com.arguments[com.inFileLoc], O_RDONLY);
+        dup2(fdin, 0);
+        }
+        if(com.outputORedirection==1){
+        fdout = open(com.arguments[com.outFileLoc], O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU|S_IRWXG|S_IRWXO);
+        dup2(fdout,1);
+        }else if(com.outputARedirection==1){
+        fdout=open(com.arguments[com.outFileLoc], O_CREAT|O_WRONLY|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
+        dup2(fdout,1);
+        }
+        pid_t pid = fork();
+        if (pid < 0) {
+                printf("\nFailed forking child..");
+                return 0;
+        } else if (pid == 0) {
+                if (execvp(com.word, com.arguments) < 0) {
+                printf("\nCould not execute command..");
+                }
+                exit(0);
+        }else{
+                wait(NULL);
+        }
+        if(com.inputRedirection==1){
+        close(fdin);
+        }
+        if(com.outputORedirection==1 || com.outputARedirection==1){
+        close(fdout);
+        }
+        return 1;
+        }
+
---
+++ project2/main.c
@@ -0,0 +1,53 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+#include<dirent.h>
+#include "shell.h"
+
+
+
+
+	int main(int argc, char **argv){
+  	char *input;
+  	char **args;
+  	int again;
+	struct simple_command com;
+  	printf("\n\n\n\t*******************welcome to my shell**************************");
+  	char* username = getenv("USER");
+  	printf("\n\n\nUSER is: @%s\n", username);
+  	printf("enter in commands and begin using the shelll\n");
+  	sleep(2);
+  	do {
+
+                input = getInput();
+                int pipe=findPipe(input);
+                if(pipe==1){
+                args=str_tok(input, "|");
+                again=pipeExecution(args);
+                }else{
+                args = str_tok(input, " ");
+                com=strParse(args);
+                if (com.word == NULL) {
+                again= 1;
+                }
+                for (int i = 0; i < numInternalCommands; i++) {
+                if (strcmp(com.word, intCommands[i]) == 0) {
+                again= internalCommands(com);
+                }
+                }
+                again=externalCommands(com);
+                }
+                free(input);
+                free(args);
+        } while (again);
+        }
+
+
+
